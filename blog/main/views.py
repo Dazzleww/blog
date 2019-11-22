500cd3ad99d9e7b1579d6245b92d0c53456e9d58
Dazzleww <dali12906@gmail.com>
2019-11-22 15:00:41 +0800

    three push

---
+++ blog/main/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def main(request):
+    '''
+    Render the main page
+    '''
+    html = '''
+    <!doctype html>
+    <html>
+    <head>
+    <title>部落格</title>
+    <meta charset="utf-8">
+    </head>
+    <body>
+    <p>這是 HTML 版的 Hello world!</p>
+    </body>
+    </html>
+    '''
+    return HttpResponse(html)
